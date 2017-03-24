6cd2ce9951854343e91865bdc66708cd174db27f
diegodev3 <dev3.seucondominio@gmail.com>
2017-03-24 14:16:26 -0300

    upload

---
+++ open-default-apps.sh
@@ -0,0 +1,9 @@
+subl && sleep 2 && ./wmov.sh mov 'Sublime' 1 2
+
+gnome-terminal && sleep 2 && ./wmov.sh mov 'dev3@dev3' 3
+
+google-chrome &
+sleep 3 && ./wmov.sh mov 'Google' 1
+
+slack &
+sleep 5 && ./wmov.sh mov 'Slack' 5
